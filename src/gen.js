d80a7a8dfb5c2c6279f92562bf24c7388aa9588c
VoQn <VoQn.tyrantist@gmail.com>
2012-06-13 01:52:03 +0900

    refactoring & enhance :: set character code generator as open API

--- src/gen.js
+++ src/gen.js
@@ -4,19 +4,6 @@ var GenerateRefference = (function(){
       method,
       C = Combinator;
 
-  charInt = function(){
-      var g = C.frequency( [
-            [ 400, C.choose( 0x41, 0x7a )],
-            [ 300, C.choose( 0x30, 0x39 )],
-            [ 150, C.choose( 0x1f, 0x2f )],
-            [ 140, C.choose( 0x7b, 0x7f )],
-            [ 7, C.choose( 0x80, 0xd7ff )],
-            [ 2, C.choose( 0xe000, 0xfffd )],
-            [ 1, C.choose( 0x10000, 0x10ffff )]
-          ] );
-      return Math.round( g() );
-  };
-
   method = {
     bool: function( ){
       var b = C.elements([ false, true ])();
@@ -33,13 +20,25 @@ var GenerateRefference = (function(){
             d = C.choose( 1, prec )();
         return Math.round( n ) / Math.round( d );
     }),
+    charcode: function(){
+       var g = C.frequency( [
+            [ 400, C.choose( 0x41, 0x7a )],
+            [ 300, C.choose( 0x30, 0x39 )],
+            [ 150, C.choose( 0x1f, 0x2f )],
+            [ 140, C.choose( 0x7b, 0x7f )],
+            [ 7, C.choose( 0x80, 0xd7ff )],
+            [ 2, C.choose( 0xe000, 0xfffd )],
+            [ 1, C.choose( 0x10000, 0x10ffff )]
+          ] );
+      return Math.round( g() );
+    },
     charator: function(){
-      var i = charInt(),
+      var i = method.charcode(),
           c = String.fromCharCode( i );
       return c;
     },
     string: function(){
-      var cs = C.listOf( charInt )(),
+      var cs = C.listOf( method.charcode )(),
           str = String.fromCharCode.apply( null, cs );
       return str;
     }
