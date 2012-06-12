e86ac6c6af7aa94648ccd05c090b3f46310b5e64
VoQn <VoQn.tyrantist@gmail.com>
2012-06-13 06:10:35 +0900

    refactoring :: seperate log string message

--- src/interface.js
+++ src/interface.js
@@ -6,14 +6,17 @@
  */
 var Interface = function( name, methods ){
   if ( arguments.length != 2 ){
-    throw new Error('Interface constructor called with ' + arguments.length + 'arguments, but expected exactly 2.');
+    throw new Error('Interface constructor called with ' +
+        arguments.length +
+        'arguments, but expected exactly 2.');
   }
   this.name = name;
   this.methods = [];
   var i = 0, l = methods.length;
   for ( ; i < l; i++ ){
     if ( typeof methods[ i ] !== 'string' ){
-      throw new Error('Interface constructor expects method names to be passed in as a string');
+      throw new Error('Interface constructor expects ' +
+          'method names to be passed in as a string');
     }
     this.methods.push( methods[ i ]);
   }
@@ -25,20 +28,25 @@ var Interface = function( name, methods ){
  */
 Interface.ensureImplements = function( object, a_interface ){
   if ( arguments.length < 2 ){
-    throw new Error('Function Interface.ensureImplements called with ' + arguments.length + 'arguments, but expected at least 2.');
+    throw new Error('Function Interface.ensureImplements called with ' +
+        arguments.length +
+        'arguments, but expected at least 2.');
   }
   var i = 1, l = arguments.length, j, m;
   for ( ; i < l; i++ ){
     var _interface = arguments[ i ];
     if ( _interface.constructor !== Interface ) {
-      throw new Error('Function Interface.ensureImplements expects arguments two and above to be instance of Interface.');
+      throw new Error('Function Interface.ensureImplements ' +
+          'expects arguments two and above to be instance of Interface.');
     }
     j = 0;
     m = _interface.methods.length;
     for ( ; j < m; j++ ){
       var method = _interface.methods[ j ];
       if ( !object[ method ] || typeof object[ method ] !== 'function' ) {
-        throw new Error('Function Interface.ensureImplements: object does not implement the ' + _interface.name + ' interface. Method ' + method + ' was not found.');
+        throw new Error('Function Interface.ensureImplements: ' +
+            'object does not implement the ' + _interface.name + ' interface. ' +
+            'Method ' + method + ' was not found.');
       }
     }
   }
