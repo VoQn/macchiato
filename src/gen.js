2476ef53f7ed54e7a47b517545c1273041897dd1
VoQn <VoQn.tyrantist@gmail.com>
2012-06-13 09:01:03 +0900

    refactoring & enhance :: generateRefference.register

--- src/gen.js
+++ src/gen.js
@@ -1,8 +1,19 @@
 
-var generateRefference = (function(){
+var generateRefference = (function( combinator ){
   var GenerateRefference = function(){};
 
+  var integerGenerator = function( n ){
+    var i = combinator.choose( (-n), n )();
+    return Math.round( i );
+  };
+
   var decimalBaseDenom = 9999999999999;
+  var numberGenerator = function( l ){
+    var b = combinator.choose( 0, ( l ) )();
+    var n = combinator.choose( (-b) * decimalBaseDenom, b * decimalBaseDenom )();
+    var d = combinator.choose( 1, decimalBaseDenom )();
+    return Math.round( n ) / Math.round( d );
+  };
 
   var charCodeHierarchy = [
     [ 400,    0x41,     0x7a ], // Alphabet
@@ -22,43 +33,49 @@ var generateRefference = (function(){
   };
 
   var charCodeGenerateTable = map( generatorLow, charCodeHierarchy );
+  var registration = function( type, callback ){
+    if ( type === 'constructor' || type == 'register' ) { // Bad Hacking
+      throw new Error('GenerateRefference.' + type + ' should not overwrite');
+    }
+    // TODO Check type signature
+    GenerateRefference.prototype[ type ] = callback;
+  };
 
-  var method = {
-    bool: function( ){
-      var b = combinator.elements([ false, true ])();
-      return b;
-    },
-    integer: combinator.sized(function( n ){
-        var i = combinator.choose( (-n), n )();
-        return Math.round( i );
-    }),
-    decimal: combinator.sized(function( _n ){
-        var b = combinator.choose( 0, ( _n ) )();
-        var n = combinator.choose( (-b) * decimalBaseDenom, b * decimalBaseDenom )();
-        var d = combinator.choose( 1, decimalBaseDenom )();
-        return Math.round( n ) / Math.round( d );
-    }),
+  GenerateRefference.prototype.register = function(/* type, callback */){
+    var args = Array.prototype.slice.call( arguments );
+    if ( args.length = 2 && typeof args[ 0 ] === 'string' && typeof args[ 1 ] === 'function' ){
+      // single entry registration
+      registration( args[ 0 ], args[ 1 ] );
+      return;
+    }
+
+    // bulk registration
+    each( function( generator, type ){
+      registration( type, generator );
+    }, args[ 0 ] );
+  };
+
+  GenerateRefference.prototype.register({
+    bool: combinator.elements( [ false, true ] ),
+    integer: combinator.sized( integerGenerator ),
+    number: combinator.sized( numberGenerator ),
     charCode: function(){
       var generator = combinator.frequency( charCodeGenerateTable );
       var code = Math.round( generator() );
       return code;
     },
-    charator: function(){
-      var code = method.charCode();
+    charactor: function(){
+      var code = GenerateRefference.prototype.charCode();
       var a_char = String.fromCharCode( code );
       return a_char;
     },
     string: function(){
-      var int_arr = combinator.listOf( method.charCode )();
+      var int_arr = combinator.listOf( GenerateRefference.prototype.charCode )();
       var str = String.fromCharCode.apply( null, int_arr );
       return str;
     }
-  };
-
-  method.number = method.decimal;
-
-  createSingleton( GenerateRefference, method );
+  });
 
   return new GenerateRefference();
-})();
+})( combinator );
 
