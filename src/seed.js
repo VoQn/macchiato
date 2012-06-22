8d11a650b4557c02ecce7fe399bdea8cf20eef94
VoQn <VoQn.tyrantist@gmail.com>
2012-06-23 03:44:58 +0900

    refactoring :: code styling

--- src/seed.js
+++ src/seed.js
@@ -1,6 +1,6 @@
 
 /** @constructor */
-function Seed(){}
+var Seed = function Seed(){};
 
 /** @type {Seed} */
 var seed = new Seed();
@@ -11,11 +11,12 @@ var seed = new Seed();
  * @return {function(number):number}
  */
 seed.linear = function( opt_a, opt_b ){
-  var a = supplement( 1, opt_a, Math.max ),
-      b = supplement( 0, opt_b );
-  return function _calc_linear( x ){
-    return a * x + b;
-  };
+  var a_ = supplement( 1, opt_a, Math.max ),
+      b_ = supplement( 0, opt_b ),
+      _calc_linear_ = function( x ){
+        return a_ * x + b_;
+      };
+  return _calc_linear_;
 };
 /**
  * @param {number=} opt_a
@@ -24,12 +25,13 @@ seed.linear = function( opt_a, opt_b ){
  * @return {function(number):number}
  */
 seed.quadratic = function( opt_a, opt_b, opt_c ){
-  var a = supplement( 1, opt_a ),
-      b = supplement( 0, opt_b ),
-      c = supplement( 0, opt_c );
-  return function _calc_quadratic( x ){
-    return a * x * x + b * x + c;
-  };
+  var a_ = supplement( 1, opt_a ),
+      b_ = supplement( 0, opt_b ),
+      c_ = supplement( 0, opt_c );
+      _calc_quadratic_ = function( x ){
+        return a_ * x * x + b_ * x + c_;
+      };
+  return _calc_quadratic_;
 };
 
 /**
@@ -38,11 +40,12 @@ seed.quadratic = function( opt_a, opt_b, opt_c ){
  * @return {function(number):number}
  */
 seed.exponent = function( opt_a, opt_b ){
-  var a = supplement( 2, opt_a, Math.max ),
-      b = supplement( 1, opt_b );
-  return function _calc_exponent( x ){
-    return Math.pow( a, ( Math.round( x * b )));
-  };
+  var a_ = supplement( 2, opt_a, Math.max ),
+      b_ = supplement( 1, opt_b );
+      _calc_exponent_ = function( x ){
+        return Math.pow( a_, ( Math.round( x * b_ )));
+      };
+  return _calc_exponent_;
 };
 
 /**
@@ -52,11 +55,12 @@ seed.exponent = function( opt_a, opt_b ){
  * @return {function(number):number}
  */
 seed.logarithm = function( opt_a, opt_b, opt_c ){
-  var a = supplement( 1, opt_a ),
-      b = supplement( 2, opt_b, Math.max ),
-      c = supplement( 0, opt_c );
-  return function _calc_logarithm( x ){
-    return Math.log( x * a ) / Math.log( b ) + c;
-  };
+  var a_ = supplement( 1, opt_a ),
+      b_ = supplement( 2, opt_b, Math.max ),
+      c_ = supplement( 0, opt_c );
+      _calc_logarithm_ = function( x ){
+        return Math.log( x * a_ ) / Math.log( b_ ) + c_;
+      };
+  return _calc_logarithm_;
 };
 
