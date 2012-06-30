ffd6a2feabc7ff7fb910f3496d3cdccd40070335
VoQn <VoQn.tyrantist@gmail.com>
2012-07-01 06:11:20 +0900

    Update :: github pages

---
+++ js/macchiato-web-util.js
@@ -0,0 +1,140 @@
+
+var eventEnd = function(){
+  if ( event.cancelBubble ){
+    event.cancelBubble = true;
+  }
+  if ( event.stopPropagation ){
+    event.stopPropagation();
+  }
+  return false;
+};
+
+// testing environments
+
+var verbose = false;
+
+var setVerbose = function(){
+  verbose = event.target.checked;
+  return eventEnd();
+};
+
+var numOnly = function(){
+  return true;
+};
+
+var clearNode = function( node ){
+  while( node.firstChild ){
+    node.removeChild( node.firstChild );
+  }
+  return node;
+};
+
+var htmlEscape = (function(){
+  var map = {
+    "<":"&lt;",
+    ">":"&gt;",
+    "&":"&amp;",
+    "'":"&#39;",
+    "\"": "&quot;"
+  };
+  return function( str ){
+    return str.replace( /<|>|&|'|"/g, function( s ){
+      return map[ s ];
+    } );
+  };
+})();
+
+var add_event = (function(){
+  if ( document.addEventListener ){
+    return function( node, type, handler ){
+      node.addEventListener( type, handler, false );
+    };
+  } else if ( document.attachEvent ) {
+    return function( node, type, handler ){
+      node.attachEvent('on' + type, function( evt ){
+        handler.call( node, evt );
+      });
+    };
+  } else {
+    return function( node, type, handler ){
+      var _handler = node[ 'on' + type ];
+      node[ 'on' + type ] = function( evt ){
+        if (_handler) {
+          _handler.call( node, evt || eindow.evt );
+        }
+        handler.call( node, evt );
+      };
+    };
+  }
+})();
+
+var add_on_load = function( callback ){
+  add_event( window, 'load', callback );
+};
+
+var scrollWithAdjust = function( anchor, header_tool_bar_id ){
+  var header = document.getElementById( header_tool_bar_id ),
+      selector = anchor.getAttribute('href'),
+      hash = selector.substring( 1 ),
+      target = document.getElementById( hash ),
+      offset, y;
+  location.hash = hash;
+  offset = header.getBoundingClientRect().height || 0;
+  y = target.getBoundingClientRect().top || 0;
+  if ( y - offset !== 0 ){
+    window.scrollBy(0, y - offset);
+  }
+  return false;
+};
+
+var setInternalAnchorBehavior = function( header_tool_bar_id ){
+  var anchors = document.getElementsByTagName( 'a' ),
+      hasInternalLink = function( anchor ){
+        return anchor.getAttribute( 'href' ).match( /^#[a-zA-Z0-9_\-]+$/i );
+      },
+      internalLinks = filter( hasInternalLink,  anchors);
+  each( function( anchor ){
+    anchor.onclick = function( ){
+        return scrollWithAdjust( anchor, header_tool_bar_id );
+    };
+  }, internalLinks );
+
+  return this;
+};
+
+var initTestConstroller = function( identifiers ){
+  /**
+   * identifiers = {
+   *   counter_id: <string>
+   *   verbose_id: <string>
+   *   runner_id: <string>
+   * }
+   */
+  var count_controller = document.getElementById( identifiers.counter_id ),
+      verbose_controller = document.getElementById( identifiers.verbose_id ),
+      runner = document.getElementById( identifiers.runner_id );
+
+  if ( count_controller.parentNode.tagName === 'label' ){
+    add_event( count_controller.parentNode, 'click', function( evt ){
+      count_controller.focus();
+    });
+  }
+
+  add_event( verbose_controller, 'click', function( evt ){
+     macchiato.setVerbose( evt.target.checked );
+     return eventEnd();
+  });
+
+  if ( verbose_controller.parentNode.tagName === 'label' ) {
+    add_event( verbose_controller.parentNode, 'click', function( evt ){
+      verbose_controller.click();
+      return eventEnd();
+    });
+  }
+
+  add_event( runner, 'click', function( evt ){
+    macchiato.taste();
+    return eventEnd();
+  });
+};
+
